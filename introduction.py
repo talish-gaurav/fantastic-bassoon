6d775a310eccca9c50f7004b59c551c6e75de92e
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-10-30 18:04:01 +0000

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,11 @@
+print("Find if your number is greter than 15")
+
+i1=int(input("Enter your amount here"))
+
+if i1>=15:
+
+     print(i1, "is greater than or equal to 15")
+
+else:
+     
+     print(i1, "is less than 15")
